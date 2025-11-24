62f9528048d3a05e93950c10681a9018db339d0c
Udaya Nath singh <udaynathsingh2003@gmail.com>
2025-11-25 01:30:40 +0530

    Delete Even_odd_zero_finder.py

--- Even_odd_zero_finder.py
+++
@@ -1,7 +0,0 @@
-number = int (input("Enter your number : "))
-if number  == 0 :
-    print ("It is zero")
-elif number % 2 == 0 :
-    print ("It is a even number  ")
-else:
-    print ("It is a odd number  ")
